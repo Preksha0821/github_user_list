6670b4c1510582e084854284a5cac0d8b1e7cd3f
preksha0821 <preksha.21893@gmail.com>
2017-08-04 15:14:05 -0500

    Retrieving user details based on input

--- user_list/src/App.js
+++ user_list/src/App.js
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import './App.css';
-//
-// const App = (props) => {
-//     return (
-//
-//             <div className="divstyle">
-//                 <img className="imgstyle" src={props.avatar_url}/>
-//                 <div className="maindiv">
-//                     <div className="namediv">{props.name}</div>
-//                     <div>{props.Company}</div>
-//                 </div>
-//             </div>
-//
-//     )
-// };
-//
-// const cardlist = (props) => {
-//     return (
-//         <div>
-//             <App
-//                 name="Dharmik"
-//                 avatar_url="https://avatars0.githubusercontent.com/u/1?v=4"
-//                 Company="Facebook">
-//             </App>
-//             <div>
-//                 {props.cards.map(card => <App {...card}/>)}
-//             </div>
-//         </div>
-//     )
-// };
-//
-// export default cardlist;
+import React from 'react';
+import './App.css';
+
