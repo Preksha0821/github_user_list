fd9ade9a85015aeaea9a177a7d3a56834491c705
preksha0821 <preksha.21893@gmail.com>
2017-08-04 12:06:44 -0500

    Retrieved users from git api

--- user_list/src/App.js
+++ user_list/src/App.js
@@ -1,72 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const App =  (props)=>{
-  return(
-      <div className="divstyle">
-          <img className="imgstyle" src={props.avatar_url}/>
-          <div className="maindiv">
-          <div className="namediv">{props.name}</div>
-          <div>{props.Company}</div>
-      </div>
-      </div>
-    )
-};
-
-
-
-let data = [
-    {
-        name:"mojombo",
-        avatar_url:"https://avatars0.githubusercontent.com/u/1?v=4",
-        Company:"Facebook"
-    },
-    {
-        name:"defunkt",
-        avatar_url:"https://avatars0.githubusercontent.com/u/2?v=4",
-        Company:"Twitter"
-    },
-    {
-        name:"pjhyett",
-        avatar_url:"https://avatars0.githubusercontent.com/u/3?v=4",
-        Company:"Google"
-    }
-];
-
-
-const cardlist = (props)=>{
-    return(
-        <div>
-            <App name="mojombo"
-                avatar_url="https://avatars0.githubusercontent.com/u/1?v=4"
-            Company="Facebook"/>
-
-
-
-        </div>
-    )
-};
-
-// class App extends Component {
-//   render() {
+// import React from 'react';
+// import './App.css';
+//
+// const App = (props) => {
 //     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
+//
+//             <div className="divstyle">
+//                 <img className="imgstyle" src={props.avatar_url}/>
+//                 <div className="maindiv">
+//                     <div className="namediv">{props.name}</div>
+//                     <div>{props.Company}</div>
+//                 </div>
+//             </div>
+//
+//     )
+// };
+//
+// const cardlist = (props) => {
+//     return (
+//         <div>
+//             <App
+//                 name="Dharmik"
+//                 avatar_url="https://avatars0.githubusercontent.com/u/1?v=4"
+//                 Company="Facebook">
+//             </App>
+//             <div>
+//                 {props.cards.map(card => <App {...card}/>)}
+//             </div>
 //         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//           <h1>
-//               <card/>
-//           </h1>
-//       </div>
-//     );
-//   }
-// }
-
-
-
-export default cardlist;
+//     )
+// };
+//
+// export default cardlist;
--- user_list/src/index.js
+++ user_list/src/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Card function for rendering data of user
+const Card = (props) => {
+        return(
+            <div className="divstyle">
+                <img className="imgstyle" src={props. avatar_url}/>
+                <div className="maindiv">
+                    <div className="namediv">{props.name}</div>
+                    <div>{props.Company}</div>
+                </div>
+            </div>
+        );
+}
+
+//Data array for storing information of users.
+let data = [
+    {
+        name: "mojombo",
+        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+        Company: "Facebook"
+    },
+    {
+        name: "defunkt",
+        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+        Company: "Twitter"
+    },
+    {
+        name: "pjhyett",
+        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+        Company: "Google"
+    },
+    {
+        name: "wycats",
+        avatar_url: "https://avatars0.githubusercontent.com/u/4?v=4s",
+        Company: "Instagram"
+    }
+];
+
+//Cardlist function for looping through the data array
+const Cardlist = (props) => {
+    return (
+        <div>
+            <div>
+                {props.cards.map(card => <Card {...card}/>)}
+            </div>
+        </div>
+    );
+};
+
+
+
+ReactDOM.render(<Cardlist cards={data}/>, document.getElementById('root'));
 registerServiceWorker();
