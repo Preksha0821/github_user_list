fde6bee140b22fd6bdf4eba56162c7edf27f0010
preksha0821 <preksha.21893@gmail.com>
2017-08-04 15:14:05 -0500

    Successfully added new card based on github username.

--- user_list/src/index.js
+++ user_list/src/index.js
@@ -2,78 +2,84 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
+import axios from 'axios';
 import registerServiceWorker from './registerServiceWorker';
+
 
 //Card function for rendering data of user
 const Card = (props) => {
-        return(
-            <div className="divstyle">
-                <img className="imgstyle" src={props. avatar_url}/>
-                <div className="maindiv">
-                    <div className="namediv">{props.name}</div>
-                    <div>{props.Company}</div>
-                </div>
+    return (
+        <div className="divstyle">
+            <img className="imgstyle" src={props.avatar_url} alt="profile"/>
+            <div className="maindiv">
+                <div className="namediv">{props.name}</div>
+                <div>{props.Company}</div>
             </div>
-        );
+        </div>
+    );
 }
 
-//Data array for storing information of users.
-let data = [
-    {
-        name: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        Company: "Facebook"
-    },
-    {
-        name: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        Company: "Twitter"
-    },
-    {
-        name: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        Company: "Google"
-    },
-    {
-        name: "wycats",
-        avatar_url: "https://avatars0.githubusercontent.com/u/4?v=4s",
-        Company: "Instagram"
-    }
-];
 
 //Cardlist function for looping through the data array
 const Cardlist = (props) => {
     return (
         <div>
             <div>
-                {props.cards.map(card => <Card {...card}/>)}
+                {props.cards.map(card => <Card key={card.id} {...card}/>)}
             </div>
         </div>
     );
 };
 //class Form created to add a new card of the user
-class Form extends React.Component{
-    render(){
-        return(
-        <form>
-            <input type="text" placeholder="Github user"/>
-            <button type="submit">Add card</button>
-        </form>
+class Form extends React.Component {
+    state = {userName: ''}
+    //handlesubmit function to submit userdata on click
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        //axios will fetch the data from url and get an object
+        axios.get(`https://api.github.com/users/${this.state.userName}`)
+            .then(resp => {
+                this.props.onSubmit(resp.data);
+                this.setState({userName: ''});
+            });
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.userName}
+                       onChange={(event) => this.setState({userName: event.target.value})} placeholder="Github user"
+                       required="required"/>
+                <button type="submit">Add card</button>
+            </form>
         );
     }
 }
 //class App created to call the form and cardlist in single class
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+    state = {
+        //Data array for storing information of users.
+        data: []
+    };
+
+    //addnewcard function to get the previous state and concat it.
+    addNewCard = (cardInfo) => {
+        this.setState(prevState => ({
+            data: prevState.data.concat(cardInfo)
+        }))
+    };
+
+    render() {
+        return (
             <div>
-                <Form/>
-                <Cardlist cards={data}/>
+                <Form onSubmit={this.addNewCard}/>
+                <Cardlist cards={this.state.data}/>
             </div>
         );
     }
 }
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
